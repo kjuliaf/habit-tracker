427761dbfd7166c60666dc60817d951041e6f315
kjuliaf <103151478+kjuliaf@users.noreply.github.com>
2025-04-03 14:03:12 +0200

    Implemented landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,60 @@
 <script lang="ts">
 	import '../style.css';
+	import '@fontsource-variable/nunito-sans';
+	import '@fontsource-variable/manrope';
+
+	let showHamburgerMenu = false;
+
+	function setShowHamburgerMenu(show: boolean) {
+		showHamburgerMenu = show;
+	}
 </script>
+
+<header class="navbar absolute z-2 flex justify-between border-b-2 border-(--border-color) p-4 sm:px-6">
+	<!-- Logo -->
+	<div class="flex items-center gap-2">
+		<!-- Alt1 -->
+		<a href="/"><img src="logo.png" alt="HabitZen Logo" width="150" /></a>
+
+		<!-- Alt2 -->
+		<!-- <img src="logo2.png" alt="HabitZen Logo" width="150" /> -->
+	</div>
+
+	<!-- Buttons for desktop -->
+	<div class="hidden gap-3 sm:flex">
+		<button class="btn btn-primary btn-outline">Register</button>
+		<button class="btn btn-primary">Log in</button>
+	</div>
+
+	<!-- Hamburger menu icon for mobile -->
+	<label class="swap swap-rotate sm:hidden">
+		<!-- This hidden checkbox controls the state -->
+		<input type="checkbox" on:click={() => setShowHamburgerMenu(!showHamburgerMenu)} />
+
+		<!-- Hamburger icon -->
+		<svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512">
+			<path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+		</svg>
+
+		<!-- Close icon -->
+		<svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512">
+			<polygon
+				points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+			/>
+		</svg>
+	</label>
+</header>
+
+<!-- Hamburger menu content -->
+<div
+	class="absolute flex {showHamburgerMenu
+		? ''
+		: 'hidden'} top-0 right-0 h-75 w-full border-b-2 border-(--border-color) bg-(--background-color) sm:hidden"
+>
+	<div class="m-auto flex w-full flex-col items-center gap-6 p-8 pt-24">
+		<button class="btn btn-primary btn-outline btn-block">Register</button>
+		<button class="btn btn-primary btn-block">Log in</button>
+	</div>
+</div>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<div class="prose p-10">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-dvh p-8 sm:p-0">
+	<div
+		class="prose prose-sm sm:prose-base prose-h1:text-[2rem] sm:prose-h1:text-[2.5rem] m-auto flex flex-col items-center py-40"
+	>
+		<!-- Landing page information -->
+		<h1 class="text-center">Build better habits and create balance with HabitZen.</h1>
+		<p class="mb-12 px-4 text-center sm:mb-14 sm:px-12">
+			Life moves fast, but true growth comes from slowing down and focusing on what truly matters. HabitZen helps you
+			build meaningful habits that bring stability, clarity, and balance to your daily life.
+		</p>
+		<button class="btn btn-lg btn-outline btn-primary">Get started →</button>
+	</div>
 </div>
