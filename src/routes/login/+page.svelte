db4ae8f4e8bd094e59eed479e72568d941c0bd40
kjuliaf <103151478+kjuliaf@users.noreply.github.com>
2025-04-11 23:59:45 +0200

    Added register and login pages

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<div class="flex h-dvh p-8 sm:p-0">
+	<div class="prose m-auto flex flex-col items-center py-40">
+		<h1>Welcome back</h1>
+		<!-- Login form -->
+		<form class="fieldset w-xs border-none bg-none">
+			<label for="email" class="fieldset-label">Email</label>
+			<input type="email" id="email" class="input" placeholder="Email" />
+			<label class="fieldset-label" for="password">Password</label>
+			<input type="password" id="password" class="input" placeholder="Password" />
+			<button type="submit" class="btn btn-neutral mt-4">Login</button>
+		</form>
+		<p class="mt-10 text-sm">Don't have an account? <a href="/register" class="text-primary">Register here</a></p>
+	</div>
+</div>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,18 @@
+<div class="flex h-dvh p-8 sm:p-0">
+	<div class="prose m-auto flex flex-col items-center py-40">
+		<h1>Create accout</h1>
+		<!-- Register form -->
+		<form class="fieldset w-xs border-none bg-none">
+			<label for="name" class="fieldset-label">Name</label>
+			<input type="text" id="name" class="input" placeholder="Name" />
+			<label for="email" class="fieldset-label">Email</label>
+			<input type="email" id="email" class="input" placeholder="Email" />
+			<label for="password" class="fieldset-label">Password</label>
+			<input type="password" id="password" class="input" placeholder="Password" />
+			<label for="confirm-password" class="fieldset-label">Confirm password</label>
+			<input type="password" id="confirm-password" class="input" placeholder="Confirm password" />
+			<button type="submit" class="btn btn-neutral mt-4">Create account</button>
+		</form>
+		<p class="mt-10 text-sm">Have an account? <a href="/login" class="text-primary">Log in here</a></p>
+	</div>
+</div>
